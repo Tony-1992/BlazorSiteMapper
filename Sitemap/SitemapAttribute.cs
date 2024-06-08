985f3bf6f8fb9449826d7842ff0f412d11f774eb
Tony <tony.p.developer@hotmail.com>
2024-06-08 18:31:11 +0100

    added documentation to enums

--- Sitemap/SitemapAttribute.cs
+++ Sitemap/SitemapAttribute.cs
@@ -1,5 +1,10 @@
 ﻿namespace BlazorSitemapper.Sitemap
 {
+
+    /// <summary>
+    /// Attribute to specify metadata for sitemap URLs, used to help search engines 
+    /// understand how often a page is updated and its priority relative to other pages.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public sealed class SitemapUrlAttribute : Attribute
     {
@@ -7,6 +12,13 @@
         public double Priority { get; }
         public string Url { get; }
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SitemapUrlAttribute"/> class.
+        /// </summary>
+        /// <param name="url">The URL of the page.</param>
+        /// <param name="changeFreq">The frequency with which the page is likely to change. Defaults to <see cref="ChangeFreq.Daily"/>.</param>
+        /// <param name="priority">The priority of the page relative to other pages on the site. Defaults to 0.5.</param>
         public SitemapUrlAttribute(string url, ChangeFreq changeFreq = ChangeFreq.Daily, double priority = 0.5)
         {
             Url = url;
